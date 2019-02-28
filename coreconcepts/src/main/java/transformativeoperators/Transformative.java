a0755553850a8a50ab941b0002cfa0418029dd8c
udayrajsawhney <udayraj.s16@iiits.in>
2019-02-28 11:02:48 +0530

    added transformative operators

--- coreconcepts/src/main/java/transformativeoperators/Transformative.java
+++ coreconcepts/src/main/java/transformativeoperators/Transformative.java
@@ -1,9 +1,8 @@
 package transformativeoperators;
 
 
-import io.reactivex.Observable;
-import io.reactivex.ObservableSource;
-import io.reactivex.ObservableTransformer;
+import io.reactivex.*;
+import io.reactivex.disposables.Disposable;
 
 public class Transformative {
 
@@ -17,21 +16,58 @@ public class Transformative {
         };
     }*/
 
-    public void transformOp(){
-        Observable.just("cat","dog","mouse","python").map(i->i.length())
-                .filter(i->i>3).subscribe(System.out::println);
+    public void transformOp() {
+        Observable.just("cat", "dog", "mouse", "python").map(i -> i.length())
+                .filter(i -> i > 3).subscribe(System.out::println);
 
-        Observable.just("cat","dog","mouse","python").compose(mapToNumber())
+        Observable.just("cat", "dog", "mouse", "python").compose(mapToNumber())
                 .subscribe(System.out::println);
     }
 
-    public static ObservableTransformer<String,Integer> mapToNumber(){
-        return upstream -> upstream.map(i->i.length()).filter(i->i>3);
+    public static ObservableTransformer<String, Integer> mapToNumber() {
+        return upstream -> upstream.map(i -> i.length()).filter(i -> i > 3);
     }
 
+    public void customOp(){
+        Observable.empty().cast(String.class).lift(defaultValue("new entry")).subscribe(System.out::println);
+    }
 
+    public static ObservableOperator<String, String> defaultValue(String value) {
+        return new ObservableOperator<String, String>() {
+            @Override
+            public Observer<? super String> apply(Observer<? super String> observer) throws Exception {
+                return new Observer<String>() {
+                    boolean empty = true;
 
-    public static void main(String[] args){
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        empty = false;
+                        observer.onNext(s);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        observer.onError(e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        if (empty){
+                            observer.onNext(value);
+                        } else {
+                            observer.onComplete();
+                        }
+                    }
+                };
+            }
+        };
+    }
+
+    public static void main(String[] args) {
         Transformative transform = new Transformative();
         transform.transformOp();
     }
